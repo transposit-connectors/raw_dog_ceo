cfe35e9c99275a4f386e04da91c0e731d9743f32
Daisie Huang <daisie@transposit.com>
2019-01-18 19:45:38 +0000

    Initial service boilerplate

---
+++ hello_world.js
@@ -0,0 +1,12 @@
+function hello_world() {
+  return [
+    {
+      language: "english",
+      message: "Hello, world"
+    },
+    {
+      language: "spanish",
+      message: "Hola, mundo"
+    }
+  ];
+}
